4749f0d96912d7683c6c92de469d5ecb9f2cb104
Daniel Ruf <daniel@daniel-ruf.de>
2012-06-27 14:48:29 +0300

    added support for firefox

--- jquery.tilt.js
+++ jquery.tilt.js
@@ -27,23 +27,29 @@
 							
 			$(base.el).parent(".tilt-parent").mousemove(function(e) {
 			
-			
-				var distanceFromCenterX = -(rectW/2) + e.offsetX;			
+			var offX = e.pageX - $(base.el).offset().left;
+			var offY = e.pageY - $(base.el).offset().top;
+				
+				var distanceFromCenterX = -(rectW/2) + offX;				
 				var normalisedDistanceX = 2* distanceFromCenterX / maxDim;
-						
-				var distanceFromCenterY = -(rectH/2) + e.offsetY;			
+				
+				var distanceFromCenterY = -(rectH/2) + offY;			
 				var normalisedDistanceY = 2* distanceFromCenterY / maxDim;
 						
 				var tiltAngleX = normalisedDistanceX * angle;
 				var tiltAngleY = -normalisedDistanceY * angle;
-			
+				//alert(tiltAngleX);	
 				$(base.el).css({"-webkit-transform": " perspective("+perspective+") rotateY("+tiltAngleX+"deg) rotateX("+tiltAngleY+"deg)", "-webkit-transform-origin": "50% 50%", "-webkit-transition": "none"});
-			
+			    $(base.el).css({"-moz-transform": " perspective("+perspective+"px) rotateY("+tiltAngleX+"deg) rotateX("+tiltAngleY+"deg)", "-moz-transform-style": "preserve-3d", "-moz-transform-origin": "50% 50%", "-moz-transition": "none"});
+			    $(base.el).css({"transform": " perspective("+perspective+") rotateY("+tiltAngleX+"deg) rotateX("+tiltAngleY+"deg)", "transform-origin": "50% 50%", "transition": "none"});
+
 			});
 			
 			$(base.el).parent(".tilt-parent").mouseout(function(e) { 
 				
 				$(base.el).css({"-webkit-transform": " perspective("+perspective+") rotateY(0deg) rotateX(0deg)", "-webkit-transform-origin": "50% 50%", "-webkit-transition": "all linear 0.5s"});
+				$(base.el).css({"-moz-transform": " perspective("+perspective+"px) rotateY(0deg) rotateX(0deg)", "-moz-transform-origin": "50% 50%", "-moz-transition": "all linear 0.5s"});
+				$(base.el).css({"transform": " perspective("+perspective+") rotateY(0deg) rotateX(0deg)", "transform-origin": "50% 50%", "transition": "all linear 0.5s"});
 				
 			});
 			
